f254ac6efc68769615326bf6ae23f5f5b481fe98
jjollivet <jeremiejollivet@gmail.com>
2022-12-14 22:27:22 +0100

    Completed day 6 part 2 challenge

--- day6/day6.py
+++ day6/day6.py
@@ -1,19 +1,22 @@
 import queue
 import re
 
+def start_of_packet(data, num_char):
+    start_pos = 0;
+    marker = data[:start_pos+num_char]
+
+    while(len(set(marker)) < num_char):
+        start_pos += 1
+        marker = data[start_pos:start_pos+num_char]
+
+    return start_pos + num_char
+
 def main():
     #Import intput file
     with open("input.txt") as my_file:
         data = my_file.read()
 
-    start_pos = 0
-    marker = data[:start_pos+4]
-
-    # Part 1 - Look for rolling set of 4 non-repeated characters
-    while(len(set(marker)) < 4):
-        start_pos += 1
-        marker = data[start_pos:start_pos+4]
-
-    print(start_pos+4)
+    print(f'Part 1 start-of-packet char: {start_of_packet(data, 4)}')
+    print(f'Part 2 start-of-packet char: {start_of_packet(data, 14)}')
 
 main()
