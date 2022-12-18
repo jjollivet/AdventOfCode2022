437449aac7ca02f6b1cda1d40716f724d91e0ab9
jjollivet <jeremiejollivet@gmail.com>
2022-12-18 14:54:49 +0100

    Completed day 11 part 1 challenge

--- day11/day11.py
+++ day11/day11.py
@@ -1,14 +1,15 @@
 import re
 
 class Monkey:
-    def __init__(self, operation, divisible_by, if_true, if_false):
+    def __init__(self, operation, divisible_by, if_true, if_false, num_inspections):
+        self.num_inspections = num_inspections
         self.operation = operation
         self.divisible_by = divisible_by
         self.if_true = if_true
         self.if_false = if_false
 
     def inspect_and_throw(self, object):
-
+        self.num_inspections += 1
         old = object
         new = eval(self.operation)
         new = new // 3
@@ -39,7 +40,7 @@ def main():
         div_by = re.findall('\d+', data[i*6+3])
         iftrue = re.findall('\d+', data[i*6+4])
         iffalse = re.findall('\d+', data[i*6+5])
-        list_of_monkeys.append(Monkey(data[i*6+2][19:], int(div_by[0]), int(iftrue[0]), int(iffalse[0])))
+        list_of_monkeys.append(Monkey(data[i*6+2][19:], int(div_by[0]), int(iftrue[0]), int(iffalse[0]),0))
 
     # Part 1
     for round in range(num_rounds):
@@ -51,6 +52,14 @@ def main():
                 throw_to, worry_level = list_of_monkeys[monkey].inspect_and_throw(item)
                 list_of_items[monkey].pop(0)
                 list_of_items[throw_to].append(worry_level)
-        print(list_of_items)
+
+    # Retreive all inspection numbers and find maximum two
+    num_inspects = []
+    for monkey in range(num_monkeys):
+        num_inspects.append(list_of_monkeys[monkey].num_inspections)
+    max_inspect = max(num_inspects)
+    num_inspects.remove(max_inspect)
+    max_inspect_2 = max(num_inspects)
+    print(f'Part 1 Monkey business is {max_inspect * max_inspect_2}')
 
 main()
